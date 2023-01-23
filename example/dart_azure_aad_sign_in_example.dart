857da16db4a1fc5ab0640a574971d677a944624f
Patrick Hettich <patrick_hettich@hotmail.com>
2023-01-23 17:21:47 +0100

    first commit

---
+++ example/dart_azure_aad_sign_in_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_azure_aad_sign_in/dart_azure_aad_sign_in.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_azure_aad_sign_in.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_azure_aad_sign_in;
+
+export 'src/dart_azure_aad_sign_in_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
