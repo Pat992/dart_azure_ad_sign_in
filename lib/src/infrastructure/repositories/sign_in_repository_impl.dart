a4b9bcf62cc7b7710d6df0f67dba707a02e4ec4e
Patrick Hettich <patrick_hettich@hotmail.com>
2023-01-23 19:33:48 +0100

    server timeout added to repository implementation

--- lib/src/infrastructure/repositories/sign_in_repository_impl.dart
+++ lib/src/infrastructure/repositories/sign_in_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:dart_azure_aad_sign_in/src/domain/entities/token_entity.dart';
 import 'package:dart_azure_aad_sign_in/src/domain/repositories/sign_in_repository.dart';
 import 'package:dart_azure_aad_sign_in/src/infrastructure/datasources/azure_api_datasource.dart';
@@ -12,36 +14,52 @@ class SignInRepositoryImpl implements SignInRepository {
   final String oauthUri;
   final String serverSuccessResponse;
   final String serverErrorResponse;
+  final Duration serverTimeoutDuration;
   final AzureApiDatasource azureApiDatasource;
   final HttpServerDatasource httpServerDatasource;
 
-  const SignInRepositoryImpl({
+  SignInRepositoryImpl({
     required this.port,
     required this.clientId,
     required this.grantType,
     required this.oauthUri,
     required this.serverSuccessResponse,
     required this.serverErrorResponse,
+    required this.serverTimeoutDuration,
     required this.azureApiDatasource,
     required this.httpServerDatasource,
   });
 
   @override
   Future<Token> signIn() async {
+    TokenModel token;
+    final future = Future.delayed(serverTimeoutDuration);
+    final timeoutStream = future.asStream();
+
+    final timeoutStreamSubscription = timeoutStream.listen((event) {
+      cancelSignIn();
+      token = TokenModel.fromMap({});
+    });
+
     await httpServerDatasource.startServer();
 
     final code = await httpServerDatasource.listenForRequest();
 
-    final token = await azureApiDatasource.getToken(code: code);
+    final tokenString = await azureApiDatasource.getToken(code: code);
 
     httpServerDatasource.stopServer();
 
-    return TokenModel.fromMap(token);
+    timeoutStreamSubscription.cancel();
+
+    token = TokenModel.fromMap(tokenString);
+
+    return token;
   }
 
   @override
   Future<Token> refreshToken({required Token token}) async {
     final tokenModel = token as TokenModel;
+
     final refreshedToken =
         await azureApiDatasource.refreshToken(refreshToken: token.refreshToken);
 
